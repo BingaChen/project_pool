b057fba7dd632a302393abe3f554aa355e6901e4
陈琪峰 <17858805466@163.com>
2018-08-30 13:23:28 +0800

    init

---
+++ app/src/test/java/com/cqf/project_pool/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+package com.cqf.project_pool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void func(){
+        System.out.print("1+2="+(1+2));
+    }
+}
