f99ae23d96480e3397ee2f17e54e82a394323629
陈琪峰 <17858805466@163.com>
2018-08-30 14:56:34 +0800

    update

--- app/src/main/java/com/cqf/project_pool/MainActivity.java
+++ app/src/main/java/com/cqf/project_pool/MainActivity.java
@@ -2,12 +2,42 @@ package com.cqf.project_pool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.cqf.fenglib.base.BaseActivity;
+import com.cqf.fenglib.base.BaseToolbarActivity;
+import com.cqf.fenglib.utils.MyUtils;
+
+public class MainActivity extends BaseToolbarActivity {
+
+    TextView tvClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void initView() {
+//        setTitle("主页面");
+        tvClick=findViewById(R.id.tvClick);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+        tvClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyUtils.showToast(context,"click!");
+            }
+        });
+    }
+
 }
